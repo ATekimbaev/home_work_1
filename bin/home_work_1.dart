cd7715cba849fa7da03447e94a01a74dd4ce6586
tekimbaev1 <tekimbaev@gmail.com>
2023-09-13 18:56:11 +0600

    commit

---
+++ bin/home_work_1.dart
@@ -0,0 +1,41 @@
+void main() {
+  //Задча 1
+
+  // print('Введите первое число');
+  // double a = double.parse(stdin.readLineSync()!);
+  // print('Введите второе число');
+  // double b = double.parse(stdin.readLineSync()!);
+  // print(
+  //     '${a + b}-Сложение\n${a - b}-Вычитание\n${a * b}-Умножение\n${a / b}-Деление');
+
+  //Задча 2
+
+  // print('Введите число');
+  // int a = int.parse(stdin.readLineSync()!);
+  // print(a.isEven);
+
+  //Задча 3
+
+  // print('Введите число');
+  // int a = int.parse(stdin.readLineSync()!);
+  // print(
+  //   '$a * 1 = ${a * 1}\n$a * 2 = ${a * 2}\n$a * 3 = ${a * 3}\n$a * 4 = ${a * 4}\n$a * 5 = ${a * 5}\n$a * 6 = ${a * 6}\n$a * 7 = ${a * 7}\n$a * 8 = ${a * 8}\n$a * 9 = ${a * 9}\n$a * 10 = ${a * 10}',
+  // );
+
+  //Задча 4
+
+  // print('Введите градусы в Целсиях');
+  // double a = double.parse(stdin.readLineSync()!);
+
+  // print('$a в Целсиях = ${(a * 9 / 5) + 32} в Фаренгейтах');
+
+  //Задча 5
+
+  // print('Введите сумму');
+  // double summ = double.parse(stdin.readLineSync()!);
+  // print('Введите название валюты');
+  // String valuta = stdin.readLineSync()!;
+  // print('Введите стоимость валюты');
+  // double price = double.parse(stdin.readLineSync()!);
+  // print('$summ $valuta = ${price * summ} сом');
+}
